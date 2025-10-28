72bd7377bf26ff2be0fc7304c04c7f04af923ee9
shevishek <shevishek@gmail.com>
2025-10-28 21:29:03 +0200

    last version

---
+++ db.js
@@ -0,0 +1,13 @@
+export const books=[
+    {id:123,name:"good",category:"jokes",price:40,status:false},
+    {id:124,name:"tolife",category:"adults",price:80,status:true},
+    {id:125,name:"doplicates",category:"scary",price:90,status:false},
+    {id:126,name:"shikupitzki",category:"jokes",price:30,status:true},
+]
+ 
+export const lends=[
+    {date:'2025-10-27',idcust:100},
+    {date:'2025-09-27',idcust:101},
+    {date:'2025-08-26',idcust:101},
+    {date:'2025-10-25',idcust:100},
+]
